8c32312b2fe587b8b2d7b6a83aadb198eb011d6b
Ross Waguespack <rosswaguespack@gmail.com>
2017-07-22 13:23:00 -0500

    Refactored added highs lows weekdays icons methods

--- lib/derecho.rb
+++ lib/derecho.rb
@@ -19,31 +19,60 @@ class Derecho
 		response["forecast"].to_a
 	end
 
-	def get_advanced_weather_data
-		daily_forecast_two = response[1][1]["forecastday"]
-		daily_forecast_two.each do |day|
-			weekday = day["date"]["weekday"]
-			condition = day["conditions"]
-			temp_high = day["high"]["fahrenheit"]
-			temp_low = day["low"]["fahrenheit"]
-			icon = day["icon"]
-			icon_img = day["icon_url"]
-
-			puts "#{weekday} will be #{condition} with a high of #{temp_high} and a low of #{temp_low}."
-		end
-	end
-
 	def get_basic_weather_data
+		results = []
 		daily_forecast = response[0][1]["forecastday"]
 		daily_forecast.each do |day|
 			if day["title"].length <= 10
 				weather = []
 				day_string = day["title"].split(' ')
-				puts "#{day_string[0]} will be #{day["fcttext"]}"
+				results.push("#{day_string[0]} will be #{day["fcttext"]}")
 			end
 		end
+		results
 	end
 
-	# Derecho.new("long beach", "ms").get_advanced_weather_data
-	# Derecho.new("new orleans", "la").get_basic_weather_data
+	def get_weekday
+		results = []
+		daily_forecast = response[1][1]["forecastday"]
+		daily_forecast.each do |day|
+			days = day["date"]["weekday"]
+
+			results.push(days)
+		end
+		results
+	end
+
+	def get_icon
+		results = []
+		daily_forecast = response[1][1]["forecastday"]
+		daily_forecast.each do |day|
+			icons = day["icon_url"]
+
+			results.push(icons)
+		end
+		results
+	end
+
+	def get_daily_highs
+		results = []
+		daily_forecast = response[1][1]["forecastday"]
+		daily_forecast.each do |day|
+			temp_high = day["high"]["fahrenheit"]
+
+			results.push(temp_high)
+		end
+		results
+	end
+
+	def get_daily_lows
+		results = []
+		daily_forecast = response[1][1]["forecastday"]
+		daily_forecast.each do |day|
+			temp_low = day["low"]["fahrenheit"]
+
+			results.push(temp_low)
+		end
+		results
+	end	
 end
